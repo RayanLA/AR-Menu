6b46580cfd1e5c079b7686dc27d13220cc3ae56e
Rayan <rayan.la-roze@u-psud.fr>
2021-04-13 17:22:02 +0200

    Début de script

---
+++ Assets/PlatAsset.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatAsset : MonoBehaviour
+{
+	public string nom = "";
+	public int prix = 0;
+	public GameObject model3D;
+	public GameObject prix;
+	public GameObject nomPlat;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    	
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
