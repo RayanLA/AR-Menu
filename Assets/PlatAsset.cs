697698be43528f5eb9168fe3a4e0daa48e76ae49
Rayan <rayan.la-roze@u-psud.fr>
2021-04-14 11:57:24 +0200

    finalisation PlatAsset + correctif

--- Assets/PlatAsset.cs
+++ Assets/PlatAsset.cs
@@ -13,15 +13,16 @@ public class PlatAsset : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-		GameObject _NomPlat = GameObject.Find("NomPlat"); 
+		//GameObject _NomPlat = gameObject.Transform.GetChild(0).gameObject;// .Find("NomPlat"); 
+		GameObject _NomPlat = gameObject.transform.Find("NomPlat").gameObject;// .Find("NomPlat"); 
     	_NomPlat.GetComponent<TextMesh>().text = nom; 
     	_NomPlat.GetComponent<TextMesh>().alignment = TextAlignment.Center; 
  
-    	GameObject _prix = GameObject.Find("Prix"); 
+    	GameObject _prix = gameObject.transform.Find("Prix").gameObject; 
     	_prix.GetComponent<TextMesh>().text = Convert.ToString(prix)+" €";  
     	_prix.GetComponent<TextMesh>().alignment = TextAlignment.Center;  
  
-    	GameObject _model3D = GameObject.Find("Model3D"); 
+    	GameObject _model3D = gameObject.transform.Find("Model3D").gameObject; 
     	//Replace existing object  
     	Quaternion rotation = _model3D.transform.rotation; 
         Vector3 position = _model3D.transform.position; 
