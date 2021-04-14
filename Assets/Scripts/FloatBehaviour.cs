25ea28c3c484e78016cadf53d129e6d2f9460b2d
Etienne <etienne.berrie@gmail.com>
2021-04-14 16:59:14 +0200

    Ajoute Script rotation

---
+++ Assets/Scripts/FloatBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class FloatBehaviour : MonoBehaviour
+{
+    private Vector3 _position;
+    
+    public float degreesPerSecond = 5.0f;
+    public float amplitude = 0.8f;
+    public float frequency = 0.1f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _position = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0f, Time.deltaTime * degreesPerSecond, 0f));
+        
+        // // Float up/down with a Sin()
+        // Vector3 tempPos = _position;
+        // tempPos.y += Mathf.Sin (Time.fixedTime * Mathf.PI * frequency) * amplitude;
+        //
+        // transform.localPosition = tempPos;
+    }
+}
