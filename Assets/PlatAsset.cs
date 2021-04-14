a47af922507a62d06a002682e70a4b0671e90f46
Rayan <rayan.la-roze@u-psud.fr>
2021-04-14 10:17:20 +0200

    finalisation platAsset

--- Assets/PlatAsset.cs
+++ Assets/PlatAsset.cs
@@ -17,14 +17,21 @@ public class PlatAsset : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-		GameObject _NomPlat = GameObject.Find("NomPlat");
+		GameObject _NomPlat = GameObject.Find("NomPlat"); 
     	_NomPlat.GetComponent<TextMesh>().text = nom; 
-    	GameObject _prix = GameObject.Find("Prix");
-    	_prix.GetComponent<TextMesh>().text = Convert.ToString(prix)+" €"; 
-    	GameObject _model3D = GameObject.Find("Model3D");
-    	_model3D = Instantiate(model3D); 
-            //Instantiate(respawnPrefab, respawn.transform.position, respawn.transform.rotation);
-        
+    	_NomPlat.GetComponent<TextMesh>().alignment = TextAlignment.Center; 
+ 
+    	GameObject _prix = GameObject.Find("Prix"); 
+    	_prix.GetComponent<TextMesh>().text = Convert.ToString(prix)+" €";  
+    	_prix.GetComponent<TextMesh>().alignment = TextAlignment.Center;  
+ 
+    	GameObject _model3D = GameObject.Find("Model3D"); 
+    	//Replace existing object  
+    	Quaternion rotation = _model3D.transform.rotation; 
+        Vector3 position = _model3D.transform.position; 
+    	GameObject temp = Instantiate(model3D, position, rotation);  
+    	temp.transform.SetParent(_model3D.transform.parent); 
+    	DestroyImmediate(_model3D); 
         
     }
 
