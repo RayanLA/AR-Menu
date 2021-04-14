e81361ecfb6b4d6c2ee7c654818f728652c2b7c8
Rayan <rayan.la-roze@u-psud.fr>
2021-04-14 09:25:34 +0200

    Modification des texts au lancement du script via params

--- Assets/PlatAsset.cs
+++ Assets/PlatAsset.cs
@@ -1,20 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PlatAsset : MonoBehaviour
 {
+	//Paramètres 
 	public string nom = "";
 	public int prix = 0;
 	public GameObject model3D;
-	public GameObject prix;
-	public GameObject nomPlat;
+
+	// Inner field
+	
 
 
     // Start is called before the first frame update
     void Start()
     {
-    	
+		GameObject _NomPlat = GameObject.Find("NomPlat");
+    	_NomPlat.GetComponent<TextMesh>().text = nom; 
+    	GameObject _prix = GameObject.Find("Prix");
+    	_prix.GetComponent<TextMesh>().text = Convert.ToString(prix)+" €"; 
+    	GameObject _model3D = GameObject.Find("Model3D");
+    	_model3D = Instantiate(model3D); 
+            //Instantiate(respawnPrefab, respawn.transform.position, respawn.transform.rotation);
+        
         
     }
 
