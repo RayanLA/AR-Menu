a54e331ad3a6c1bce30a1421656600a6620183c6
Etienne <etienne.berrie@gmail.com>
2021-05-07 17:46:12 +0200

    Add Dish Template

---
+++ Assets/Dish Objects/DishScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DishScript : MonoBehaviour
+{
+    //Paramètres 
+    public string nom = "<noname>";
+    public double prix = 0;
+    public GameObject prefab;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        prefab.transform.localScale = new Vector3(1, 1, 1);
+        GameObject o = Instantiate(prefab, new Vector3(0,0,0), Quaternion.identity);
+
+        
+        
+        
+        //Texte
+        // var theText = new GameObject();
+        // var textMesh = theText.AddComponent<TextMesh>();
+        // var meshRenderer = theText.AddComponent<MeshRenderer>();
+        // textMesh.text = dishName;
+        //     
+        //     
+        // var name = dishName;
+        // TextMesh name3d = new TextMesh();
+        // name3d.text = name;
+        //
+        // Instantiate(theText, new Vector3(0, 0, (float)2), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
