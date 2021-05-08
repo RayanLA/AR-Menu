8910f99a97975f7e28170fb7b5260ea0343fdd78
Etienne <etienne.berrie@gmail.com>
2021-05-08 11:39:04 +0200

    Add Dish ramen

--- Assets/Dish Objects/DishScript.cs
+++ Assets/Dish Objects/DishScript.cs
@@ -12,12 +12,20 @@ public class DishScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        prefab.transform.localScale = new Vector3(1, 1, 1);
-        GameObject o = Instantiate(prefab, new Vector3(0,0,0), Quaternion.identity);
+        //prefab.transform.localScale = new Vector3(1, 1, 1);
 
-        
-        
-        
+        // Change la taille
+        //Vector3 targetSize = Vector3.one * 10.0f;
+
+        //Debug.Log(prefab.gameObject.GetComponent<Renderer>().bounds);
+        // Mesh m = prefab.GetComponent<MeshFilter>().sharedMesh;
+        // Bounds meshBounds = m.bounds;
+        // Vector3 meshSize = meshBounds.size;
+        // float xScale = targetSize.x / meshSize.x * 100;
+        // float yScale = targetSize.y / meshSize.y * 100;
+        // float zScale = targetSize.z / meshSize.z * 100;
+        // prefab.transform.localScale = new Vector3(xScale, yScale, zScale);
+
         //Texte
         // var theText = new GameObject();
         // var textMesh = theText.AddComponent<TextMesh>();
@@ -30,6 +38,8 @@ public class DishScript : MonoBehaviour
         // name3d.text = name;
         //
         // Instantiate(theText, new Vector3(0, 0, (float)2), Quaternion.identity);
+        Vector3 position = gameObject.transform.position;
+        GameObject o = Instantiate(prefab, position, Quaternion.identity);
     }
 
     // Update is called once per frame
