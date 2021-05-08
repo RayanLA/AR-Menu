6f43c7cb41490af6a5fe9e7f7cdd524138640aba
Etienne <etienne.berrie@gmail.com>
2021-05-08 13:21:14 +0200

    Add Menu list

---
+++ Assets/Menu/MenuListScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuListScript : MonoBehaviour
+{
+    public List<DishScript> dishList;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.setPositions();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void setPositions()
+    {
+        Vector3 nextPosition = gameObject.transform.localPosition;
+        float width = 10;
+        float gap = 0.5f;
+
+        for (int i = 0; i < dishList.Count; i++)
+        {
+            var currentDish = dishList[i];
+            // currentDish.transform.position = new Vector3(nextPosition, 0, 0);
+            Instantiate(currentDish, nextPosition, Quaternion.identity);
+            nextPosition.x += (width + gap);
+        }
+    }
+}
